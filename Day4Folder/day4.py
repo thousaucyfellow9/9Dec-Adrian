bc9ee52d989937622dd132a330c22bf9e8a7dfb9
thousaucyfellow9 <adriantan2010@gmail.com>
2024-12-12 04:42:38 +0000

    day 4

--- Day4Folder/day4.py
+++ Day4Folder/day4.py
@@ -2,3 +2,15 @@
 # REMEMBER to change main.py to import this file.
 # You can COMMENT out the previous task before going on to the next task
 print("hello from day4")
+
+plate_is_clean = False
+print("i want to eat chicken rice today")
+import random
+
+while plate_is_clean == False:
+    print("take a mouth of chicken rice")
+    random_number = random.randint(1,6)
+    if random_number == 6:
+        plate_is_clean = True
+
+print("i have finished my chicken rice")
--- Day4Folder/exercise1.py
+++ Day4Folder/exercise1.py
@@ -10,3 +10,27 @@ You can set a price for each item.
 3. Keep asking the customer until the customer says "enough"
 4. Calculate the total purchase of the customer
 '''
+candy = 1
+chocolate = 2
+can_drinks = 2
+total = 0
+
+answer = input("Are you buying anything?")
+while answer == "yes":
+    item = input("What are you buying today?")
+    quantity = input("How many of each item?")
+    quantity = int(quantity)
+    if item == "candy":
+        price = candy * quantity
+        total = total + price
+    if item == "chocolate":
+        price = chocolate * quantity
+        total = total + price
+    if item == "can_drinks":
+        price = can_drinks * quantity
+        total = total + price
+    answer = input("Anything else?")
+print("The total is $", total)
+print("thank you for visiting NTUC")
+
+
